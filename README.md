8c11e611ec3e6faf130850d302e7960b89970f09
Harshit Kumar Singh <harshitks2203@gmail.com>
2018-03-08 02:38:55 +0530

    added new pages

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# Flutter_Quizz
-Just a basic quiz application made with Flutter Framework.
+<p align="center">
+  <b style="font-size: 30px;">QuizzApp</b><br>
+  <i>A basic True/False application made with <br>the help of Google's Flutter Framework</i><br><br>
+  <a href="https://github.com/feross/standard">
+    <img src="https://img.shields.io/badge/code_style-standard-blue.svg?style=flat-square" />
+  </a>
+  <a href="https://raw.githubusercontent.com/thecodepad/codepad/master/LICENSE"> 
+    <img src="https://img.shields.io/badge/license-MIT-lightgrey.svg?style=flat-square" />
+  </a>
+  <br><br>
+</p>
+
+
+## Screenshot
+
+[<img alt='QuizzHomePage' src="https://raw.githubusercontent.com/harshitks2203/Flutter_Quizz/master/resources/1MainScreen.png?token=APLQiRXfkVTSOT2O89-16F5sJKuGotjXks5aqY7QwA%3D%3D">](HomePage)
+
+[<img alt='QuizzQuestionPage' src="https://raw.githubusercontent.com/harshitks2203/Flutter_Quizz/master/resources/2QuestionPage.png?token=APLQicWJDrevpbDxE19Hn28ocTOY4Wpjks5aqYuxwA%3D%3D">](QuestionPage)
+
+[<img alt='QuizzAnswerPage' src="https://raw.githubusercontent.com/harshitks2203/Flutter_Quizz/master/resources/3AnswerPage.png?token=APLQiULFq6ZFD5ns1d68bLrnSbS8bClLks5aqYuxwA%3D%3D">](AnswerPage)
+
+[<img alt='QuizzScorePage' src="https://raw.githubusercontent.com/harshitks2203/Flutter_Quizz/master/resources/4ScorePage.png?token=APLQiV_9a9-HAyWgVZQCPbJX8Ko89gdPks5aqYuywA%3D%3D">](ScorePage)
+<br>
+
+## Prerequisite
+First install Flutter in your Machine. For that checkout - [Flutter documentation](https://flutter.io/).
+
+<br>
+
+## Run From Source 
+
+You can run the app via command line by following these steps and make sure your device(iOS/Android Phone) is connected:
+
+```
+$ git clone https://github.com/harshitks2203/Flutter_Quizz.git
+$ cd Flutter_Quizz
+$ flutter run
+```
+
+
+## Credits
+
+Designed and developed by: [@harshitks2203](https://www.github.com/harshitks2203) 
--- lib/pages/quiz_page.dart
+++ lib/pages/quiz_page.dart
@@ -4,6 +4,9 @@ import '../utils/question.dart';
 import '../utils/quiz.dart';
 import '../UI/answer_button.dart';
 import '../UI/question_text.dart';
+import '../UI/correct_wrong_overlay.dart';
+
+import './score_page.dart';
 
 class QuizPage extends StatefulWidget {
   @override
@@ -11,17 +14,70 @@ class QuizPage extends StatefulWidget {
 }
 
 class QuizPageState extends State<QuizPage> {
+
+  Question currentQuestion;
+  Quiz quiz = new Quiz([
+    new Question("Lightning never strikes in the same place twice?", false),
+    new Question("If you cut earthworm in half, both halves can regrow their body?", false),
+    new Question("Adults have fewer bones than babies do?", true),
+    new Question("If you cry in space, the tears just stick to your face?", true),
+    new Question("Human can distinguish between over a trillion different smells?", true),
+    new Question("Birds are dinosaurs?", true),
+    new Question("Napolean Bonapate was extremely short?", false),
+    new Question("There are more cells of bacteria in your body than there are human cells?", true),
+    new Question("GoldFish only have a memory of three seconds?", false),
+    new Question("Your fingernails and hair keep growing after you die?", false),
+    new Question("Neil Armstrong was the first man to urinate on the moon?", false),
+    new Question("Human can't breathe and swallow at the same time?", true)
+  ]);
+  String questionText;
+  int questionNumber;
+  bool isCorrect;
+  bool overlayShouldBeVisible = false;
+
+  void handleAnswer(bool answer) {
+    isCorrect = (currentQuestion.answer == answer);
+    quiz.answer(isCorrect);
+    this.setState(() {
+      overlayShouldBeVisible = true;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    currentQuestion = quiz.nextQuestion;
+    questionText = currentQuestion.question;
+    questionNumber = quiz.questionNumber;
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Stack(
+      fit: StackFit.expand,
       children: <Widget>[
         new Column( //this is our main page
           children: <Widget>[
-            new AnswerButton(true, () => print("You answered true!") ),
-            new QuestionText("Sun rises in East", 1),
-            new AnswerButton(false, () => print("You answered false!") ),
+            new AnswerButton(true, () => handleAnswer(true) ),     //true button
+            new QuestionText(questionText, questionNumber),
+            new AnswerButton(false, () => handleAnswer(false) ),   //false button
           ],
-        )
+        ),
+        overlayShouldBeVisible == true ? new CorrectWrongOverlay(
+          isCorrect,
+          () {
+            if(quiz.length == questionNumber) {
+              Navigator.of(context).pushAndRemoveUntil(new MaterialPageRoute(builder: (BuildContext context) => new ScorePage(quiz.score, quiz.length)), (Route route) => route == null);
+              return;
+            }
+            currentQuestion = quiz.nextQuestion;
+            this.setState(() {
+              overlayShouldBeVisible = false;
+              questionText = currentQuestion.question;
+              questionNumber = quiz.questionNumber;
+            });
+          }
+        ) : new Container()
       ],
     );
   }
